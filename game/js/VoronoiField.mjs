2eec4edd8845753a0f8a6839b97b000424fb6beb
Unknown <ataque992@hotmail.com>
2019-11-10 17:44:58 -0500

    Add ability to add cells with mouse click

--- game/js/VoronoiField.mjs
+++ game/js/VoronoiField.mjs
@@ -3,7 +3,7 @@ import { VoronoiCell } from './VoronoiCell.mjs'
 import * as THREE from './libs/thr.js';
 
 function VoronoiField(scene) {
-  const voronoiCount = 20;
+  const voronoiCount = 50;
   const cells = [];
 
   const field = new THREE.Object3D();
@@ -16,11 +16,18 @@ function VoronoiField(scene) {
     color.offsetHSL(1.0/voronoiCount, 0, 0);
     let x = Math.random()*6 - 3;
     let y = Math.random()*3 - 1.5;
+
+    newCell(color, x, y);
+  }
+
+  scene.add(field);
+
+  function newCell(color, x, y) {
     let cell = new VoronoiCell(scene, color, x, y)
 
     cells.push(cell);
-
     field.attach(cell.mesh);
+    return cell
   }
 
   this.update = (deltaTime, elapsedTime) => { 
@@ -30,10 +37,7 @@ function VoronoiField(scene) {
   }
 
   this.onMouseClick = (event) => {
-    console.log(field.position);
-    console.log("goes to");
-    console.log(event.position);
-    field.position.copy(event.position);
+    newCell(0x000000, event.position.x, event.position.y)
   }
 }
 
